9a6e3d09aaa91a9ce70bff522fadcab3527c8313
bern1094 <bernardodg@outlook.com>
2025-06-21 14:17:54 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -76,5 +76,9 @@ def home():
     """
     return render_template_string(html, ema=ema, fda=fda)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
